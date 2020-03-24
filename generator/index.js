5b4919d25ce6a8561d20c81b7b6782f70751a3c5
lihui <lihui@ebupt.com>
2020-03-24 13:41:20 +0800

    xiugai

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,59 @@
+// generator
 module.exports = (api, options, rootOptions) => {
+  // 删除 vue-cli3 默认的 /src 和 /public 目录
+  api.render(files => {
+    Object.keys(files)
+      .filter(path => path.startsWith('src/') || path.startsWith('public/'))
+      .forEach(path => delete files[path])
+  })
 
-  // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('../template')
+  // 安装一些基础公共库
+  api.extendPackage({
+    dependencies: {
+      "axios": "^0.19.0",
+      "babel-plugin-transform-remove-console": "^6.9.4",
+      "babel-polyfill": "^6.26.0",
+      "core-js": "^2.6.5",
+      "echarts": "^4.2.1",
+      "element-ui": "^2.12.0",
+      "font-awesome": "4.7.0",
+      "image-webpack-loader": "^6.0.0",
+      "lodash": "^4.17.4",
+      "v-charts": "^1.19.0",
+      "vue": "^2.6.10",
+      "vue-baidu-map": "^0.21.22",
+      "vue-i18n": "^8.14.0",
+      "vue-router": "^3.0.3",
+      "vuex": "^3.0.1",
+      "xlsx": "^0.15.1"
+    },
+    devDependencies: {
+      "@vue/cli-plugin-babel": "^3.11.0",
+      "@vue/cli-service": "^3.11.0",
+      "babel-plugin-transform-remove-console": "^6.9.4",
+      "image-webpack-loader": "^6.0.0",
+      "node-sass": "^4.9.0",
+      "sass-loader": "^7.1.0",
+      "vue-template-compiler": "^2.6.10"
+    }
+  });
+  // 根据自定义模板生成项目结构
+  api.render('../template/template_pc')
 
+  // 判断模板样式
+  if (options.style == '顶部导航栏模板') {
+
+    // 注入对应文件
+    api.render({
+      './src/components/Head.vue': '../template/template_pc_style/HeadTop.vue'
+    });
+
+  } else if (options.style == '侧边导航栏模板') {
+
+    // 注入对应文件
+    api.render({
+      './src/components/Head.vue': '../template/template_pc_style/HeadLeft.vue'
+    });
+
+  }
 }
--- template/template_pc/vue.config.js
+++ template/template_pc/vue.config.js
@@ -34,8 +34,6 @@ module.exports = {
             template: 'src/views/index/index.html',
             // 在 dist/index.html 的输出
             filename: 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: 'Login Page',
             // 在这个页面中包含的块，默认情况下会包含
             // 提取出来的通用 chunk 和 vendor chunk。
@@ -48,8 +46,6 @@ module.exports = {
             template: 'src/views/login/index.html',
             // 在 dist/index.html 的输出
             filename: process.env.NODE_ENV === 'production' ? 'login/index.html' : 'login.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: 'Login Page',
             // 在这个页面中包含的块，默认情况下会包含
             // 提取出来的通用 chunk 和 vendor chunk。
@@ -62,8 +58,6 @@ module.exports = {
             template: 'src/views/admin/index.html',
             // 在 dist/index.html 的输出
             filename: process.env.NODE_ENV === 'production' ? 'admin/index.html' : 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: 'Index Page',
             // 在这个页面中包含的块，默认情况下会包含
             // 提取出来的通用 chunk 和 vendor chunk。
